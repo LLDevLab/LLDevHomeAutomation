bd1de8fbab1e362687a98cf4d903ff033c80829c
Low-Level Development Lab <41891215+LLDevLab@users.noreply.github.com>
2021-05-26 19:19:02 +0300

    1_03.sql to 1_02.sql
    Moving latest database changes to 1_02 version file

--- Database/sql_v1_02/1_02.sql
+++ Database/sql_v1_02/1_02.sql
@@ -53,3 +53,13 @@ CREATE INDEX "fki_fki_ChartUnitMapping_MeasurementUnits_fk"
 
 ALTER TABLE public."Sensors"
     ALTER COLUMN "IsActive" DROP DEFAULT;
+	
+DROP INDEX public."SensorId_EventDateTime_Idx";
+
+CREATE UNIQUE INDEX "SensorId_EventDateTime_Idx"
+    ON public."SensorEvents" USING btree
+    ("SensorId" ASC NULLS LAST, "EventDateTime" DESC NULLS LAST)
+    TABLESPACE pg_default;
+
+ALTER TABLE public."SensorEvents"
+    CLUSTER ON "SensorId_EventDateTime_Idx";
